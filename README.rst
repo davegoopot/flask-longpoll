feec7e4562564e0a5601664667556ea2e5585f88
davegoopot <dave@goopot.co.uk>
2014-09-28 15:45:01 +0100

    Working version and README file

---
+++ README.rst
@@ -0,0 +1,18 @@
+
+========================
+Flask Long-Poll Example
+========================
+
+This is a little throw-away code to understand how to do chunked response http
+requests using Flask and python3.
+
+To run 
+
+vagrant up
+vagrant ssh
+
+cd /vagrant
+python3 longpoll.py
+
+
+Then back on the host OS connect your browser to http://localhost:8889
--- longpoll.py
+++ longpoll.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, Response
+import time
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    def reply_slowly():
+        yield "Please be patient.  I'm counting up to 20\n"
+        for i in range(1,21):
+            time.sleep(1)
+            yield str(i) + "\n"
+    return Response(reply_slowly(), mimetype="text/plain")
+            
+    
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
